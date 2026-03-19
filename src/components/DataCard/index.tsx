becf053c2b14a1adeabb733f0b417955ee30dc7b
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-03-19 09:41:23 -0300

    Product price toFixed

--- src/components/DataCard/index.tsx
+++ src/components/DataCard/index.tsx
@@ -13,7 +13,7 @@ export default function DataCard({ products }: Props) {
                 products.map(product => (
                     <div key={product.id} className="data-card">
                         <p className="product-name" >{product.name}</p>
-                        <p className="product-price" >R$ {product.price}</p>
+                        <p className="product-price" >R$ {product.price.toFixed(2)}</p>
                     </div>
                 ))
             }
